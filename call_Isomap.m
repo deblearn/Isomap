af366a0d64aea3e002227ec91db7637bb4686411
Debbrata saha <debbrata@unm.edu>
2018-03-22 14:03:51 -0600

    little change at call_Isomap file

--- call_Isomap.m
+++ call_Isomap.m
@@ -1,6 +1,5 @@
-x = importdata('ABIDE_fMRI_Data.txt');
-%X_data = x;
-X_data = (X_data(1:10,:));
+X_data = importdata('Mnist_data.txt');
+%X_data = (X_data(1:10,:));
 D = squeeze(sqrt(sum(bsxfun(@minus,X_data,reshape(X_data',1,size(X_data,2),size(X_data,1))).^2,2)));
 %D = L2_distance1(X_data', X_data', 1);
 % Both upper and lower D will be performed same
