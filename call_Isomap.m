d94fe62eedccf5e482caaef3716b84ffa198be4d
Debbrata saha <debbrata@unm.edu>
2018-03-22 13:49:14 -0600

    Isomap code

---
+++ call_Isomap.m
@@ -0,0 +1,12 @@
+x = importdata('ABIDE_fMRI_Data.txt');
+%X_data = x;
+X_data = (X_data(1:10,:));
+D = squeeze(sqrt(sum(bsxfun(@minus,X_data,reshape(X_data',1,size(X_data,2),size(X_data,1))).^2,2)));
+%D = L2_distance1(X_data', X_data', 1);
+% Both upper and lower D will be performed same
+
+
+%x = load('swiss_roll_data');
+%D = L2_distance(X_data(:,1:1000), X_data(:,1:1000), 1);
+options.dims = 1:2;
+[Y, R, E] = Isomap(D, 'k', 7, options); 
